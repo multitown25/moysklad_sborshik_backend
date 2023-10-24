57a10e3f2d3b7b4e83b92d931a25aca467446c40
Ramazan <62503301+multitown25@users.noreply.github.com>
2023-10-24 16:13:26 +0300

    app use static commit

--- app.js
+++ app.js
@@ -24,6 +24,9 @@ app.use(cors({
     origin: process.env.CLIENT_URL
 }));
 
+const root = path.join(__dirname, '../../var/www/build');
+app.use(express.static(root));
+
 // app.use('/', (req, res, next) => {
 //     // res.setHeader(
 //     //     'Access-Control-Allow-Origin',
